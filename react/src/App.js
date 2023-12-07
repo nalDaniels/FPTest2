0114f7ad40de85421bc8d737d759af58c93ffad7
Dwayne Toler (he/him) <84260551+djtoler@users.noreply.github.com>
2023-12-06 22:56:00 -0500

    Update App.js

--- react/src/App.js
+++ react/src/App.js
@@ -16,7 +16,7 @@ import Quiz from "./afterLogin/Quiz";
 import "./App.scss";
 
 function App() {
-  const URL = "44.204.48.174:8000";
+  const URL = "";
   console.log(URL, "<---URL")
   let tempValue = false;
   if (JSON.parse(localStorage.getItem("IsAuth")) !== null) {
